88f70a352b3ae2147678662a0e855bb18a2df8b4
OToNaShiAKi <1362446747@qq.com>
2020-05-22 19:12:04 +0800

    优化隔离

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
